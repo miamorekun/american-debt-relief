3aed8c2d22526a9c809bdb9a2b005991bb13f658
Rostyslav Hubariev <r.gubarev1303@gmail.com>
2025-02-20 11:24:32 +0200

    Smooth autoplay for reviews added

--- scripts/reviews.js
+++ scripts/reviews.js
@@ -1,22 +1,30 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-const swiper = new Swiper('.reviews', {
-    slidesPerView: "auto",
-    spaceBetween: 16,
-
-    freeMode: {
-        enabled: true,
-    },
-});
-
 function updateSwiperClass() {
     const swiperContainer = document.querySelector('.reviews');
     if (window.innerWidth > 780) {
         swiperContainer.classList.add('swiper-no-swiping');
+        const swiper = new Swiper('.reviews', {
+            spaceBetween: 52,
+        });
     } else {
         swiperContainer.classList.remove('swiper-no-swiping');
+        const swiper = new Swiper('.reviews', {
+            spaceBetween: 16,
+            freeMode: true,
+            loop: true,
+            slidesPerView: 'auto',
+            speed: 5000,
+            effect: 'slide',
+            autoplay: {
+                delay: 0,
+                pauseOnMouseEnter: false,
+                disableOnInteraction: false,
+            },
+        });
     }
 }
 
 updateSwiperClass();
 window.addEventListener('resize', updateSwiperClass);
+
